65549d551fabca2019c65bd805dbb9f957c46114
t4r3k <142579274+machine-moon@users.noreply.github.com>
2024-08-18 00:42:16 -0400

    an automated quick commit and push (tpush)

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define the text to append to ~/.bashrc
+TEXT_TO_APPEND="
+#-----------------------------------------------
+source_me() {
+    local dir=\"\$1\"
+    if [ -d \"\$dir\" ]; then
+        for file in \"\$dir\"/*; do
+            if [ -f \"\$file\" ]; then
+               # echo \"Sourcing \$file\"
+                . \"\$file\"
+            fi
+        done
+    fi
+}
+
+source_me \"\$HOME/bash\"
+source_me \"\$HOME/bash/functions\"
+"
+
+# Append the text to ~/.bashrc
+echo "$TEXT_TO_APPEND" >> ~/.bashrc
+
+# Optional: Inform the user
+echo "The function source_me has been appended to ~/.bashrc."
+
